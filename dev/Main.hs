55cebf697be144e946b570823355b2de6a80b4c3
mep2g17 <mep2g17@soton.ac.uk>
2019-02-27 18:02:00 +0000

    fucking finally :')

--- dev/Main.hs
+++ dev/Main.hs
@@ -3,6 +3,7 @@ import Grammar
 import System.Environment
 import Control.Exception
 import System.IO
+import System.IO.Unsafe (unsafePerformIO)
 
 
 main :: IO ()
@@ -19,27 +20,59 @@ noParse e = do let err =  show e
                return ()
 
 parseThis s = eval1_findMain $ parseCalc $ alexScanTokens s
+parseThisFile = do sourceText <- readFile "SampleCode.txt"
+                   let x = parseThis sourceText
+                   putStrLn $ show x
+                   return ()
 
 data M = MInt String Int | MBool String Bool deriving (Show, Eq)
 type E = [M]
 
-envInit :: FuncDeclaration_ -> E
-envInit (NormalFuncDeclaration _ vars _) = envInitInner vars
-                                         where envInitInner :: FuncBodyInitArea_ -> E
-                                               envInitInner EmptyInitArea = []
-                                               envInitInner (SingleInitArea (VarIntInit_ (Var_ name _) value)) = [(MInt name value)]
-                                               envInitInner (SingleInitArea (VarBoolInit_ (Var_ name _) value)) = [(MBool name value)]
-                                               envInitInner (MultipleInitArea (VarIntInit_ (Var_ name _) value) next) = (MInt name value) : envInitInner next
-                                               envInitInner (MultipleInitArea (VarBoolInit_ (Var_ name _) value) next) = (MBool name value) : envInitInner next
+envInit :: FuncBodyInitArea_ -> E
+envInit EmptyInitArea = []
+envInit (SingleInitArea (VarIntInit_ (Var_ name _) value)) = [(MInt name value)]
+envInit (SingleInitArea (VarBoolInit_ (Var_ name _) value)) = [(MBool name value)]
+envInit (MultipleInitArea (VarIntInit_ (Var_ name _) value) next) = (MInt name value) : envInit next
+envInit (MultipleInitArea (VarBoolInit_ (Var_ name _) value) next) = (MBool name value) : envInit next
 
+envContains :: E -> String -> Bool
+encContains [] _ = False
+envContains ((MInt name _) : vv) name' | name == name' = True
+                                       | otherwise = envContains vv name'
+envContains ((MBool name _) : vv) name' | name == name' = True
+                                        | otherwise = envContains vv name'
+
+envUpdateOrAppend :: E -> M -> E
+envUpdateOrAppend [] (MInt name' value') = [(MInt name' value')]
+envUpdateOrAppend ((MInt name value):xs) (MInt name' value') | name == name' = (MInt name value') : xs
+                                                             | otherwise = (MInt name value) : (envUpdateOrAppend xs (MInt name' value'))
 
 eval1_findMain :: [FuncDeclaration_] -> E -- FuncDeclaration_
-eval1_findMain (MainFuncDeclaration (SingleSegue funcname):ss) = evalFunction (envInit func) func
-                                                                 where func = findFunctionByName funcname ss
+eval1_findMain (MainFuncDeclaration (SingleSegue funcname):ss) = evalFunction (envInit initArea) exp
+                                                                 where (NormalFuncDeclaration fname initArea exp) = findFunctionByName funcname ss
+eval1_findMain ((MainFuncDeclaration (MultipleSegue funcname next)):ss) = evalFunction (envInit initArea) exp -- TODO!
+                                                                          where (NormalFuncDeclaration fname initArea exp) = findFunctionByName funcname ss
 
 findFunctionByName :: String -> [FuncDeclaration_] -> FuncDeclaration_
 findFunctionByName funcName ((NormalFuncDeclaration funcName' a b):ff) | funcName == funcName' = (NormalFuncDeclaration funcName' a b)
                                                                        | otherwise = findFunctionByName funcName ff
 
-evalFunction :: E -> FuncDeclaration_ -> E
-evalFunction x y = x
+evalFunction :: E -> Exp_ -> E
+evalFunction env (EqualsExp (EqualsInOut match out)) = matchUpdateEnv env (matchVarsToVarnameList match) matchIntFromStdio
+
+matchUpdateEnv :: E -> [String] -> [Int] -> E
+matchUpdateEnv env [] _ = env
+matchUpdateEnv env (n:nn) (i:ii) = matchUpdateEnv (envUpdateOrAppend env (MInt n i)) nn ii
+matchUpdateEnv env _ _ = env -- error state
+
+matchVarsToVarnameList :: Match_ -> [String]
+matchVarsToVarnameList EmptyMatch = []
+matchVarsToVarnameList EOFMatch = [] -- ?????????
+matchVarsToVarnameList (SingleMatch (Var_ name _)) = [name]
+matchVarsToVarnameList (MultipleMatch (Var_ name _) next) = name : matchVarsToVarnameList next
+
+matchIntFromStdio :: [Int]
+matchIntFromStdio = unsafePerformIO matchIntFromStdio_inner
+                    where matchIntFromStdio_inner = do line <- getLine
+                                                       return $ (map read $ words line :: [Int])
+
